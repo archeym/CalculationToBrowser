f991fc98d6ed2e9defd6944f5f5f2c71bd9014d7
Arkadijs Makarenko <arkadijs.makarenko@gmail.com>
2017-03-20 10:49:44 +0800

    Initial Commit

---
+++ Week1Assessment-Archie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Week1Assessment-Archie
+//
+//  Created by Arkadijs Makarenko on 20/03/2017.
+//  Copyright © 2017 ArchieApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
